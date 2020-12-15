b90aeb11688cadc57fe280206215f0a9f22cb891
Artemiy <rendrom@gmail.com>
2020-12-15 11:43:05 +0800

    feat: add RestApi service

---
+++ services/RestApi.ts
@@ -0,0 +1,91 @@
+import { template } from '../utils/template';
+import { Http } from './Http';
+
+export interface RestApiOptions {
+  http?: Http;
+  name?: string;
+  urls?: Record<string, string>;
+  crossDomain?: boolean;
+}
+
+export abstract class RestApi<T = Record<string, any>> {
+  name!: string;
+  urls: Record<string, string> = {
+    getMany: '/api/{name}',
+    getOne: '/api/{name}/{id}',
+    create: '/api/{name}',
+    delete: '/api/{name}/{id}',
+    update: '/api/{name}/{id}',
+  };
+
+  protected crossDomain = true;
+  protected http!: Http;
+  constructor(options: RestApiOptions = {}) {
+    if (options.http) {
+      this.http = options.http;
+    }
+    if (options.name) {
+      this.name = options.name;
+    }
+    // if (this.http === undefined) {
+    //   throw new Error('RestApi options `http` is not defined');
+    // }
+    // if (this.name === undefined) {
+    //   throw new Error('RestApi options `name` is not defined');
+    // }
+    if (options.urls) {
+      Object.assign(this.urls, options.urls);
+    }
+    if (options.crossDomain !== undefined) {
+      this.crossDomain = options.crossDomain;
+    }
+  }
+
+  prepareUrl(
+    url: string,
+    params: Record<string, number | string> = {}
+  ): string {
+    if (this.urls[url]) {
+      return template(this.urls[url], { ...params, name: this.name });
+    }
+    return url;
+  }
+
+  getOne(id: string | number): Promise<T> {
+    return this.http.request(this.prepareUrl('getOne', { id }), {
+      method: 'GET',
+      crossDomain: this.crossDomain,
+    });
+  }
+
+  getMany(params: Record<string, any> = {}): Promise<T[]> {
+    return this.http.request(this.prepareUrl('getMany'), {
+      method: 'GET',
+      crossDomain: true,
+      params,
+    });
+  }
+
+  delete(id: string | number): Promise<void> {
+    return this.http.request(this.prepareUrl('delete', { id }), {
+      method: 'DELETE',
+      crossDomain: this.crossDomain,
+    });
+  }
+
+  create(body: Partial<T>): Promise<void> {
+    return this.http.request(this.prepareUrl('create'), {
+      method: 'POST',
+      body,
+      crossDomain: this.crossDomain,
+    });
+  }
+
+  update(id: string | number, body: Partial<T>): Promise<void> {
+    return this.http.request(this.prepareUrl('update', { id }), {
+      method: 'PUT',
+      body,
+      crossDomain: this.crossDomain,
+    });
+  }
+}
--- services/UserApi.ts
+++ services/UserApi.ts
@@ -2,23 +2,24 @@ import { Credentials } from '../interfaces/Credentials';
 import { UserApi as UserApiInterface } from '../interfaces/UserApi';
 import { template } from '../utils/template';
 import { Http } from './Http';
+import { RestApi } from './RestApi';
 
 export class UserApi<UserInfo = Record<string, any>>
+  extends RestApi<UserInfo>
   implements UserApiInterface {
-  urls = {
-    login: '/api/login',
-    logout: '/api/logout',
-    userInfo: '/api/current_user',
-    list: '/api/user',
-    user: '/api/user/{id}',
-    create: '/api/user',
-    delete: '/api/user/{id}',
-    update: '/api/user/{id}',
-    oAuth2: '/api/oauth2/user?code={code}',
-    oAuth2Options: '/api/oauth2/options',
-  };
-
-  constructor(private http: Http) {}
+  constructor(http: Http) {
+    super({
+      http,
+      name: 'user',
+      urls: {
+        login: '/api/login',
+        logout: '/api/logout',
+        userInfo: '/api/current_user',
+        oAuth2: '/api/oauth2/user?code={code}',
+        oAuth2Options: '/api/oauth2/options',
+      },
+    });
+  }
 
   login(body: Credentials): Promise<void> {
     return this.http.request(this.urls.login, {
@@ -35,13 +36,6 @@ export class UserApi<UserInfo = Record<string, any>>
     });
   }
 
-  getUser(id: string | number): Promise<UserInfo> {
-    return this.http.request(template(this.urls.user, { id }), {
-      method: 'GET',
-      crossDomain: true,
-    });
-  }
-
   logout(): Promise<void> {
     return this.http.request(this.urls.logout, {
       method: 'GET',
@@ -49,33 +43,9 @@ export class UserApi<UserInfo = Record<string, any>>
     });
   }
 
-  listUsers(params: Record<string, any>): Promise<UserInfo[]> {
-    return this.http.request(this.urls.list, {
-      method: 'GET',
-      crossDomain: true,
-      params,
-    });
-  }
-
   deleteUser(id: string | number): Promise<void> {
     return this.http.request(template(this.urls.delete, { id }), {
       method: 'DELETE',
-      crossDomain: true,
-    });
-  }
-
-  createUser(body: Partial<UserInfo>): Promise<void> {
-    return this.http.request(this.urls.create, {
-      method: 'POST',
-      body,
-      crossDomain: true,
-    });
-  }
-
-  updateUser(id: string | number, body: Partial<UserInfo>): Promise<void> {
-    return this.http.request(template(this.urls.update, { id }), {
-      method: 'PUT',
-      body,
       crossDomain: true,
     });
   }
